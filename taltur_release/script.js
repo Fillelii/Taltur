e3157d91ac6d056f3a088bfd8f3db3312baf0f9b
Fillelii <73694256+Fillelii@users.noreply.github.com>
2024-04-22 19:15:54 +0300

    Recurring and new speaker-entries
    unfinnished, might not work as intended.

--- taltur_release/script.js
+++ taltur_release/script.js
@@ -2,10 +2,20 @@ var internalId = 0;
 var activeId = 0;
 var defaultBgImg = "url(https://images.unsplash.com/photo-1707343843437-caacff5cfa74?q=80&w=3475&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
 
+let recurringId = [""]; /*Array logging recurring delegate turns*/
 
+let queueNew = [""] /*Queue of New entries*/
+let queueRec = [""] /*Queue of Recurring entries*/
+
+function removeFromQueue(queueId) {
+    
+    let id = queue.indexOf($queueId);
+}
 
 //function generateBox() {
 //}
+
+
 
 // clearing the textbox after each submission;
 
@@ -23,22 +33,47 @@ function addBox() {
   var delegatNr = document.getElementById("delegatNr").value;
   var createBox = document.createElement("div");
   createBox.setAttribute("id", internalId);
-  createBox.setAttribute("class", "box");
-  createBox.innerHTML = '<div class="close_btn" onclick="this.parentNode.remove()">X</div><p>'+delegatNr+'</p>';
-  document.getElementById("box_grid").appendChild(createBox);
-  internalId++;
-  clearInput();
+  
+    
+    // Checking if delegate has already spoken
+    
+    if (recurringId.includes(delegatNr) && !queueRec.includes(delegatNr)) {
+        /*A delegate that has already had a turn*/
+        
+        queueRec.push(delegatNr);
+        //recurringId.push(delegatNr);
+        createBox.setAttribute("class", "box");
+        createBox.innerHTML = '<div class="close_btn" onclick="this.parentNode.remove()">X</div><p>'+delegatNr+'</p>';
+        document.getElementById("box_grid_2").appendChild(createBox);
+    }
+    
+    if (!recurringId.includes(delegatNr) && !queueNew.includes(delegatNr))  {
+        /*A new delegate that has not yet had a turn*/
+              
+        queueNew.push(delegatNr);
+        recurringId.push(delegatNr);
+        createBox.setAttribute("class", "box");
+        createBox.innerHTML = '<div class="close_btn" onclick="this.parentNode.remove();">X</div>   <p>'+delegatNr+'</p>';
+        document.getElementById("box_grid_1").appendChild(createBox);
+        internalId++;
+
+    clearInput();
+
     writeProgress();
+    }
+
+
 }
+
 
 //Delete box at end of taltur
 
-function delBox() {
+function delBox1() {
   
     
 
     
-  var toRemove = document.getElementById("box_grid").firstChild;
+  var toRemove = document.getElementById("box_grid_1").nthChild(2);
   toRemove.remove();
   activeId++;
 
